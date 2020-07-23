34cb7d839a173f1d6307809f22e88844ba4d7b16
meeday <59993824+meeday@users.noreply.github.com>
2020-07-23 17:05:58 +0100

    mongoose connection setup

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// routes
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}`);
+})
