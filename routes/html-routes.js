ae50634a4f2c50897810ad823d80cc1759e86e24
meeday <59993824+meeday@users.noreply.github.com>
2020-07-26 14:36:51 +0100

    html route for stats page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,8 @@ module.exports = (app) => {
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
   });
-}
+
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
