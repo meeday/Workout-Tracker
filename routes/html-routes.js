cf9489c43b13b25fd54dd7c43d67d7c7472c4a2f
meeday <59993824+meeday@users.noreply.github.com>
2020-07-26 15:38:08 +0100

    html routes comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,13 @@
+// require path to use relative routes to html files
 const path = require("path");
 
 module.exports = (app) => {
+  // routes to handle clients request for files in the public folder. Called when 'continue workout' & 'new workout' is clicked
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
   });
 
+  // this one is called when client clicks the 'fitness tracker dashboard' link on the top left corner of the page
   app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
