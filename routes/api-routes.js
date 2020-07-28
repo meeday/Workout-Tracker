edbbec7eb95b16853f981fe17131c27e4aef42e5
meeday <59993824+meeday@users.noreply.github.com>
2020-07-28 14:32:45 +0100

    async await used

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,48 +4,46 @@ const db = require("../models/workout-model");
 // get, post, put request routes setup based on the api.js file
 module.exports = (app) => {
   // get request to fetch the last workout
-  app.get("/api/workouts", (req, res) => {
-    db.find({})
-    .then(result => {
+  app.get("/api/workouts", async (req, res) => {
+    try {
+      const result = await db.find({});
       res.json(result);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+    } catch (error) {
+      res.json(error);
+    }
   });
-// post request to create new workout and save to database
+  // post request to create new workout and save to database
   app.post("/api/workouts", async (req, res) => {
     try {
       const response = await db.create({
-        type: "workout"
-      })
+        type: "workout",
+      });
       res.json(response);
     } catch (error) {
-      console.log(`workout creation failed: ${error}`);  
+      console.log(`workout creation failed: ${error}`);
     }
   });
 
   // put request to update workout exercises with a specific id
-  app.put("/api/workouts/:id", (req, res) => {
+  app.put("/api/workouts/:id", async (req, res) => {
     const { id } = req.params;
-
-    
-    db.updateOne({_id: id}, {$push: {exercises: req.body}})
-    .then(updated => {
+    try {
+      const updated = await db.updateOne(
+        { _id: id },
+        { $push: { exercises: req.body } }
+      );
       res.json(updated);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-   });
+    } catch (error) {
+      res.json(error);
+    }
+  });
   // request to workouts in range
-  app.get("/api/workouts/range", (req, res) => {
-    db.find({})
-    .then(result => {
+  app.get("/api/workouts/range", async (req, res) => {
+    try {
+      const result = await db.find({});
       res.json(result);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+    } catch (error) {
+      res.json(error);
+    }
   });
 };
